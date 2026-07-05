abda085aab5d924e93ac658ffd3eef82e6dc98f8
Ahmed Sahlol <ahmedsahlol69@gmail.com>
2026-07-05 10:15:03 +0300

    Update PST.cpp

--- Data Structure /PST.cpp
+++ Data Structure /PST.cpp
@@ -1,53 +1,86 @@
 struct Node {
-    Node *l, *r;
-    int val = 0;
-
-    Node() : l(nullptr), r(nullptr) {}
-
-    Node(int val) : l(nullptr), r(nullptr), val(val) {}
-
-    Node(Node *l, Node *r) : l(l), r(r) {
-        if (l) val += l->val;
-        if (r) val += r->val;
-    }
+    int left = 0, right = 0, val = 0;
+    Node(){}
+    Node (int v) : val(v){}
 };
-
 struct PST {
-    int n;
 
-    PST(int n) : n(n) {}
+    vector<Node> nodes;
+    int L, R;
 
-    Node merge(const Node &x, const Node &y) {
-        Node ret;
-        ret.val = x.val + y.val;
-        return ret;
+    PST(int l, int r) {
+        L = l;
+        R = r;
+        nodes.reserve(1e7);
+        nodes.push_back({});
     }
 
-    Node *build(vector<int> &a, int lx, int rx) {
-        if (lx == rx)return new Node(a[lx]);
+    void merge(Node &ans, const Node & lf, const Node & ri) {
+        ans.val = lf.val + ri.val;
+    }
+
+    int create_node() {
+        nodes.push_back({});
+        return nodes.size() - 1;
+    }
+
+    int set(int idx, int val, int ni, int lx, int rx) {
+        int id = create_node();
+        if(rx - lx == 1) {
+            nodes[id] = Node(nodes[ni].val + val);
+            return id;
+        }
+
         int mid = (lx + rx) >> 1;
-        return new Node(build(a, lx, mid), build(a, mid + 1, rx));
+
+        nodes[id].left = nodes[ni].left;
+        nodes[id].right = nodes[ni].right;
+
+        if(idx < mid)
+            nodes[id].left = set(idx, val, nodes[ni].left, lx, mid);
+        else
+            nodes[id].right = set(idx, val, nodes[ni].right, mid, rx);
+
+        merge(nodes[id], nodes[nodes[id].left], nodes[nodes[id].right]);
+        return id;
     }
 
-    Node *build(vector<int> &a) { return build(a, 0, n - 1); }
+    int set(int idx, int val, int version) {
+        return set(idx, val, version, L, R + 1);
+    }
 
-    Node *set(Node *v, int idx, int val, int lx, int rx) {
-        if (lx == rx)return new Node(val);
+    Node get(int l, int r, int ni, int lx, int rx) {
+        if(ni == 0) return nodes[ni];
+        if(lx >= r || rx <= l)
+            return Node();
+        if(lx >= l && rx <= r)
+            return nodes[ni];
+
         int mid = (lx + rx) >> 1;
-        if (idx <= mid)return new Node(set(v->l, idx, val, lx, mid), v->r);
-        return new Node(v->l, set(v->r, idx, val, mid + 1, rx));
+
+        Node ans = Node();
+        merge(ans, get(l, r, nodes[ni].left, lx, mid), get(l, r, nodes[ni].right, mid, rx));
+        return ans;
+    }
+    int get(int l, int r, int version) {
+        return get(l, r, version, L, R + 1).val;
     }
 
-    Node *set(Node *v, int idx, int val) { return set(v, idx, val, 0, n - 1); }
-
-    Node query(Node *v, int l, int r, int lx, int rx) {
-        if (rx < l || lx > r)return Node();
-        if (l <= lx && rx <= r)return *v;
+    int get_kth(int k, int node_l, int node_r, int lx, int rx) {
+        if(nodes[node_r].val - nodes[node_l].val < k)
+            return -1;
+        if(rx - lx == 1)
+            return lx;
         int mid = (lx + rx) >> 1;
-        Node left = query(v->l, l, r, lx, mid);
-        Node right = query(v->r, l, r, mid + 1, rx);
-        return merge(left, right);
+        int cnt = nodes[nodes[node_r].left].val - nodes[nodes[node_l].left].val;
+        if (cnt >= k) {
+            return get_kth(k, nodes[node_l].left, nodes[node_r].left, lx, mid);
+        } else {
+            return get_kth(k - cnt, nodes[node_l].right, nodes[node_r].right, mid, rx);
+        }
     }
 
-    Node query(Node *v, int l, int r) { return query(v, l, r, 0, n - 1); }
+    int get_kth(int k, int node_l, int node_r) {
+        return get_kth(k, node_l, node_r, L, R + 1);
+    }
 };
